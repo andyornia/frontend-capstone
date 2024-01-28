9e5f9e24cf81eb583c6f85d952fa880c9790e32d
EC2 Default User <ec2-user@ip-172-31-22-243.us-west-1.compute.internal>
2024-01-28 06:21:50 +0000

    css for general layout

--- src/Main.js
+++ src/Main.js
@@ -3,15 +3,19 @@ import React from 'react';
 const Main = () => {
     return (
         <main>
-        <section>
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <p>
-                We are a family owned Mediterranean restaurant, 
-                focused on traditional recipes served with a modern twist.
-            </p>
-             <button>Reserve a Table</button>
+        <section id="about-section">
+            <div id="about-info">
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <p>
+                    We are a family owned Mediterranean restaurant, 
+                    focused on traditional recipes served with a modern twist.
+                </p>
+                 <button>Reserve a Table</button>
+            </div>
+            <div id="about-img">
              <img src="./images/restauranfood.jpg" alt="a plate of our Mediterranean sandwiches" width={200} />
+            </div>
         </section>
         
         <section>
