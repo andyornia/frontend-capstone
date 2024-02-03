a101e97a140c5ff513179e7b66cb245283ca0bfb
EC2 Default User <ec2-user@ip-172-31-22-243.us-west-1.compute.internal>
2024-02-03 05:21:08 +0000

    hamburger menu

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Nav = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  }
+  
   return (
     <nav id="main-nav">
-      <ul>
+      <div className={`menu-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu} >
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
+      <ul className={`nav-list ${isOpen ? 'show' : ''}`}>
         <li><a href="index.html">Home</a></li>
         <li><a href="index.html">About</a></li>
         <li><a href="index.html">Menu</a></li>
