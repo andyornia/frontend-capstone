2a7a7355f1c6dcc7464a1931bedc74e076752968
EC2 Default User <ec2-user@ip-172-31-22-243.us-west-1.compute.internal>
2024-02-20 04:30:09 +0000

    booking page test added for changing the date and for submitting the form

--- src/__tests__/BookingPage.test.js
+++ src/__tests__/BookingPage.test.js
@@ -1,29 +1,25 @@
-import { render, screen, fireEvent, waitFor, jest } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor, jest, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App'; // Assuming your App component is in the parent directory
 
 
-test('Clicking on a link navigates to the About page and finds an element by ID', async () => {
+test('Loading the reservations page shows a drop down menu from the initializeTimes function', async () => {
 
-  // Render your component
-  render(<App />); // Assuming your App component renders a link to the About page
+  // Render the application
+  render(<App />);
 
-  // Simulate clicking on the link
-  // You can use fireEvent or userEvent to simulate the click event
-  // For example:
-  // fireEvent.click(screen.getByText('About')); // Using fireEvent
-  userEvent.click(screen.getAllByText('Reservations')[0]); // Using userEvent
-
+  // Simulate clicking on the reservations page
+  // this will trigger the initializeTimes function as part of the call to fetchAPI()
+  act(() => {
+    userEvent.click(screen.getAllByText('Reservations')[0]);
+  });
+  
   // Wait for the page to stabilize and the desired element to appear
   await waitFor(() => {
     expect(screen.getByText('Make a Reservation')).toBeInTheDocument();
   }, {timeout: 5000});
   
-  const buttonElement = screen.getByText("Reserve");
-  expect(buttonElement).toBeInTheDocument();
-
-
-  // Wait for the page to stabilize and the desired element to appear
+  // Wait for the page to load the available times from the API
   await waitFor(() => {
     expect(screen.getByText('22:00')).toBeInTheDocument();
   }, {timeout: 5000});
@@ -45,23 +41,81 @@ test('Clicking on a link navigates to the About page and finds an element by ID'
 
   // Assert that onSelect function is called with the selected value
   expect(selectElement).toHaveValue('14:00');
-
-  fireEvent.click(screen.getByText('Reserve'));
   
-  await waitFor(() => {
-    const optionElement14 = screen.getByText('14:00');
-    // Assert that the option element has the class "unavailable"
-    expect(optionElement14).toHaveTextContent('14:00');
-    // expect(optionElement14).toHaveClass('unavailable');
-    
-    const optionElement11 = screen.getByText('11:00');
-    // Assert that the option element has the class "unavailable"
-    expect(optionElement11).toHaveTextContent('11:00');
-    // expect(optionElement11).not.toHaveClass('unavailable');    
+});
+
+
+test('Changing the date on the reservations page re-populates the list of available times using the updateTimes function', async () => {
+
+  // Render the application
+  render(<App />);
+
+  // Simulate clicking on the reservations page
+  // this will trigger the initializeTimes function as part of the call to fetchAPI()
+  act(() => {
+    userEvent.click(screen.getAllByText('Reservations')[0]);
   });
   
-  // test updating times by changing the date
-  // Simulate selecting an available option and reserving
+  // Wait for the page to stabilize and the desired element to appear
+  await waitFor(() => {
+    expect(screen.getByText('Make a Reservation')).toBeInTheDocument();
+  }, {timeout: 5000});
+  
+  // Wait for the page to load the available times from the API
+  await waitFor(() => {
+    expect(screen.getByText('22:00')).toBeInTheDocument();
+  }, {timeout: 5000});
+  
+  // Find the select element
+  const selectElement = screen.getByTestId('reservationDate');
 
+  // Simulate selecting an option
+  fireEvent.change(selectElement, { target: { value: '2024-02-18' } });
+
+  // Wait for the page to load the available times from the API
+  await waitFor(() => {
+    expect(screen.getByText('22:00')).toBeInTheDocument();
+    const options = [... screen.getByTestId('reservationTime')];
+    
+    expect(options).toHaveLength(13); // Assuming there are 12 options
+    // Assert that each option has the correct text content
+    const optionTexts = ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+    options.forEach((option, index) => {
+      expect(option).toHaveTextContent(optionTexts[index]);
+    });
+  }, {timeout: 5000});
+  
+});
+
+
+
+test('Form submission takes you to the reservation confirmed page', async () => {
+
+  // Render the application
+  render(<App />);
+
+  // Simulate clicking on the reservations page
+  // this will trigger the initializeTimes function as part of the call to fetchAPI()
+  act(() => {
+    userEvent.click(screen.getAllByText('Reservations')[0]);
+  });
+  
+  // Wait for the page to stabilize and the desired element to appear
+  await waitFor(() => {
+    expect(screen.getByText('Make a Reservation')).toBeInTheDocument();
+  }, {timeout: 5000});
+  
+  // Wait for the page to load the available times from the API
+  await waitFor(() => {
+    expect(screen.getByText('22:00')).toBeInTheDocument();
+  }, {timeout: 5000});
+  
+  // Find the select element
+  const reserveButton = screen.getByText('Reserve');
+  fireEvent.submit(reserveButton);
+
+  await waitFor(() => {
+    expect(screen.getByText('Reservation Confirmed'));
+  });
   
 });
